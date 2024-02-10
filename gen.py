029bccdd9880be3f49c8bda58208dacda732bdb1
XO DEV <xodump123@gmail.com>
2024-02-10 00:01:48 -0600

    fix emails

--- gen.py
+++ gen.py
@@ -56,22 +56,22 @@ def gen_email_3(first_name, last_name, fake_domain):
     return email
 
 def gen_email_4(first_name, last_name, fake_domain):
-    return f"{first_name}{RandomWord().word()}{randint(10, 999)}@{fake_domain}"
+    return f"{first_name}{RandomWord().word()}{randint(10, 999)}@{fake_domain}".lower()
 
 def gen_email_5(first_name, last_name, fake_domain):
     first_initial = first_name[:1]
-    return f"{first_initial}{last_name}{randint(10, 999)}@{fake_domain}"
+    return f"{first_initial}{last_name}{randint(10, 999)}@{fake_domain}".lower()
 
 def gen_email_6(first_name, last_name, fake_domain):
     last_initial = last_name[:1]
-    return f"{first_name}{last_initial}{randint(10, 999)}@{fake_domain}"
+    return f"{first_name}{last_initial}{randint(10, 999)}@{fake_domain}".lower()
 
 def gen_email_7(first_name, last_name, fake_domain):
     return gen_email_8(first_name, last_name, fake_domain)
 
 
 def gen_email_8(first_name, last_name, fake_domain):
-    return f"{RandomWord().word()}{last_name}{randint(10, 999)}@{fake_domain}"
+    return f"{RandomWord().word()}{last_name}{randint(10, 999)}@{fake_domain}".lower()
 
 # TODO:add weights to each one
 gen_email_methods = [gen_email, gen_email_2, gen_email_3, gen_email_4, gen_email_5, gen_email_6, gen_email_7, gen_email_8]
